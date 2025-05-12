17f9c49f2d9432602607f487b45ff4122b81ebd8
Luciano Massoni <massoni.luciano7@gmail.com>
2025-05-12 18:56:42 -0300

    hice las funciones de update, delete y findById de ProductoRepositoryImpl

--- src/main/java/com/negocio/adris/model/repositories/ProductoRepositoryImpl.java
+++ src/main/java/com/negocio/adris/model/repositories/ProductoRepositoryImpl.java
@@ -31,7 +31,7 @@ public class ProductoRepositoryImpl implements ProductoRepository {
           preparedStatement.setDouble(5, p.getCosto());
           preparedStatement.setDouble(6, p.getGanancia());
           preparedStatement.setDouble(7, p.getPrecio());
-          preparedStatement.setString(8,p.getTipo().toString());
+          preparedStatement.setString(8,p.getTipo().name());
           preparedStatement.setDate(9, Date.valueOf(p.getFechaVencimiento()));
 
           preparedStatement.executeUpdate();
@@ -49,59 +49,116 @@ public class ProductoRepositoryImpl implements ProductoRepository {
 
     @Override
     public void update(Producto p) {
+            String sql = """
+                    UPDATE Producto SET
+                        nombre = ?,
+                        marca = ?,
+                        peso = ?,
+                        cantidad = ?,
+                        costo = ?,
+                        ganancia = ?,
+                        precio = ?,
+                        tipo = ?,
+                        fecha_vencimiento = ?
+                    WHERE id = ?
+                """;
+
+            try(Connection conn = DBConnection.getConnection();
+                PreparedStatement preparedStatement = conn.prepareStatement(sql)) {
+                preparedStatement.setString(1, p.getNombre());
+                preparedStatement.setString(2, p.getMarca());
+                preparedStatement.setDouble(3, p.getPeso());
+                preparedStatement.setInt(4, p.getCantidad());
+                preparedStatement.setDouble(5, p.getCosto());
+                preparedStatement.setDouble(6, p.getGanancia());
+                preparedStatement.setDouble(7, p.getPrecio());
+                preparedStatement.setString(8, p.getTipo().name());
+                preparedStatement.setDate(9, Date.valueOf(p.getFechaVencimiento()));
+                preparedStatement.setLong(10, p.getId());
+
+                preparedStatement.executeUpdate();
+
+
+            } catch (SQLException e) {
+                throw new RuntimeException("Error al actualizar el producto" + e.getMessage(), e);
+            }
 
     }
 
     @Override
     public void delete(int id) {
+        String sql = "DELETE FROM Producto WHERE id = ?";
 
+        try(Connection conn = DBConnection.getConnection();
+            PreparedStatement preparedStatement = conn.prepareStatement(sql)) {
+            preparedStatement.setInt(1, id);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException("Error al eliminar un producto por Id" + e.getMessage(), e);
+        }
     }
 
     @Override
     public Producto findById(int id) {
-        return null;
+        String sql = "SELECT * FROM Producto WHERE id = ?";
+        Producto producto = null;
+
+        try(Connection conn = DBConnection.getConnection();
+            PreparedStatement preparedStatement = conn.prepareStatement(sql)){
+            preparedStatement.setInt(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+
+            if (resultSet.next()){
+                TipoProducto tipo = TipoProducto.valueOf(resultSet.getString("tipo")); // Simplificado
+                LocalDate localDate = resultSet.getDate("fecha_vencimiento").toLocalDate();
+
+                 producto = new Producto(
+                     resultSet.getInt("id"),
+                     resultSet.getString("nombre"),
+                     resultSet.getString("marca"),
+                     resultSet.getDouble("peso"),
+                     resultSet.getInt("cantidad"),
+                     resultSet.getDouble("costo"),
+                     resultSet.getDouble("ganancia"),
+                     resultSet.getDouble("precio"),
+                     tipo,
+                     localDate
+                 );
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Error al encontrar un producto por Id" + e.getMessage(), e);
+        }
+        return producto;
+
     }
 
     @Override
     public List<Producto> findAll() {
         List<Producto> productos = new ArrayList<>();
-
-        String sql = """
-                    SELECT * FROM Producto
-                """;
+        String sql = "SELECT * FROM Producto";
 
         try(Connection conn = DBConnection.getConnection();
             Statement statement = conn.createStatement();
-            ResultSet resultSet = statement.executeQuery(sql))
-        {
+            ResultSet resultSet = statement.executeQuery(sql)){
 
-            TipoProducto tipo = null;
             while (resultSet.next()){
-                for (TipoProducto tipoProducto : TipoProducto.values()) {
-                    if (tipoProducto.toString().equals(resultSet.getString("tipo"))){
-                         tipo = tipoProducto;
-                    }
-                }
-
-                Date sqlDate = resultSet.getDate("fecha_vencimiento");
-                LocalDate localDate = sqlDate.toLocalDate();
-
                 Producto p = new Producto(
-                        resultSet.getInt("id"),
-                        resultSet.getString("nombre"),
-                        resultSet.getString("marca"),
-                        resultSet.getDouble("peso"),
-                        resultSet.getInt("cantidad"),
-                        resultSet.getDouble("costo"),
-                        resultSet.getDouble("ganancia"),
-                        resultSet.getDouble("precio"),
-                        tipo,
-                        localDate
+                    resultSet.getInt("id"),
+                    resultSet.getString("nombre"),
+                    resultSet.getString("marca"),
+                    resultSet.getDouble("peso"),
+                    resultSet.getInt("cantidad"),
+                    resultSet.getDouble("costo"),
+                    resultSet.getDouble("ganancia"),
+                    resultSet.getDouble("precio"),
+                    TipoProducto.valueOf(resultSet.getString("tipo")),
+                    resultSet.getDate("fecha_vencimiento").toLocalDate()
                 );
                 productos.add(p);
             }
         } catch (SQLException e) {
-            throw new RuntimeException("Error al obtener todos los productos" + e);
+            throw new RuntimeException("Error al obtener todos los productos" + e.getMessage(), e);
         }
 
         return productos;
