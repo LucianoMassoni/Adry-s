5a7bd6701b367056b410076ebeb37c5439bc68bd
Luciano Massoni <massoni.luciano7@gmail.com>
2025-05-19 16:41:31 -0300

    creación de ProductoNotFoundException

---
+++ src/main/java/com/negocio/adris/model/exceptions/ProductoNotFoundException.java
@@ -0,0 +1,7 @@
+package com.negocio.adris.model.exceptions;
+
+public class ProductoNotFoundException extends Exception {
+    public ProductoNotFoundException(String message){
+        super(message);
+    }
+}
