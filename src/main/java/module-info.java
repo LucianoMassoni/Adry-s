33f50b5565b4e81e9688fb98ce26e2c6a13e4c81
Luciano Massoni <massoni.luciano7@gmail.com>
2025-05-19 16:47:21 -0300

    configurar validations e ijects en module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,17 @@ module com.negocio.adris {
     requires javafx.fxml;
 
     requires com.dlsc.formsfx;
+    requires java.sql;
+    requires jakarta.validation;
+    requires jakarta.inject;
+    requires com.google.guice;
+    requires org.hibernate.validator;
+    opens com.negocio.adris.model.dtos to org.hibernate.validator;
 
     opens com.negocio.adris to javafx.fxml;
     exports com.negocio.adris;
+
+    exports com.negocio.adris.model.repositories;
+    exports com.negocio.adris.model.service;
+    exports com.negocio.adris.model.entities;
 }
