33bafefc92a46a8d9a2791434e8dd46c88d50993
Luciano Massoni <massoni.luciano7@gmail.com>
2025-05-23 21:27:13 -0300

    agregue un binding de la base de datos en AppModule para usar inyección de dependencia.

--- src/main/java/com/negocio/adris/config/AppModule.java
+++ src/main/java/com/negocio/adris/config/AppModule.java
@@ -1,6 +1,7 @@
 package com.negocio.adris.config;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.Singleton;
 import com.negocio.adris.model.repositories.ProductoRepository;
 import com.negocio.adris.model.repositories.ProductoRepositoryImpl;
 import com.negocio.adris.model.service.ProductoService;
@@ -8,11 +9,21 @@ import com.negocio.adris.model.service.ProductoServiceImpl;
 import jakarta.validation.Validation;
 import jakarta.validation.Validator;
 
+import java.sql.Connection;
+
 public class AppModule extends AbstractModule {
     @Override
     protected void configure(){
-        bind(ProductoRepository.class).to(ProductoRepositoryImpl.class);
+        // Configuración de repositorios
+        bind(ProductoRepository.class).to(ProductoRepositoryImpl.class).in(Singleton.class);
+
+        // Configuración de servicios
         bind(ProductoService.class).to(ProductoServiceImpl.class);
+
+        // Configuración de Validator
         bind(Validator.class).toProvider(() -> Validation.buildDefaultValidatorFactory().getValidator());
+
+        // Configuración de base de datos   
+        bind(Connection.class).toProvider(DBConnection.class);
     }
 }
