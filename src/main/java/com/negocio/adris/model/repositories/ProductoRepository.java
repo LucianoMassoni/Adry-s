1db050ed8e0dbb238b6376ab70c3cf97bfdcb6a6
Luciano Massoni <massoni.luciano7@gmail.com>
2025-05-23 21:21:12 -0300

    findById en ProductoRepository tira un ProductoNotFoundException

--- src/main/java/com/negocio/adris/model/repositories/ProductoRepository.java
+++ src/main/java/com/negocio/adris/model/repositories/ProductoRepository.java
@@ -1,13 +1,14 @@
 package com.negocio.adris.model.repositories;
 
 import com.negocio.adris.model.entities.Producto;
+import com.negocio.adris.model.exceptions.ProductoNotFoundException;
 
 import java.util.List;
 
 public interface ProductoRepository {
     void save(Producto p);
-    void update(Producto p); //TODO: me faltaría un throws ProductoNotFoundException
+    void update(Producto p);
     void delete(int id);
-    Producto findById(int id);
+    Producto findById(int id) throws ProductoNotFoundException;
     List<Producto> findAll();
 }
