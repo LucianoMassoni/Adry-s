dcab39d5ecb6695dc3212a60d010ab9ccac777a5
Luciano Massoni <massoni.luciano7@gmail.com>
2025-05-23 21:29:17 -0300

    DBConnection ahora implementa un Provider de Connection.

--- src/main/java/com/negocio/adris/config/DBConnection.java
+++ src/main/java/com/negocio/adris/config/DBConnection.java
@@ -1,13 +1,21 @@
 package com.negocio.adris.config;
 
+import com.google.inject.Provider;
+import com.google.inject.Singleton;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class DBConnection {
+public class DBConnection implements Provider<Connection> {
     private static final String URL = "jdbc:sqlite:fiambreria.db";
 
-    public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL);
+    @Override
+    public Connection get() {
+        try {
+            return DriverManager.getConnection(URL);
+        } catch (SQLException e) {
+            throw new RuntimeException("Error al conectar a la BD" + e.getMessage() + e);
+        }
     }
 }
