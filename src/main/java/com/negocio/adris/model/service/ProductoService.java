57439c4c8ef2f936bdd58cb639dbbe0d8abdc956
Luciano Massoni <massoni.luciano7@gmail.com>
2025-05-26 17:09:17 -0300

    Le agregue los metodos de modificarProducto, obtenerTodosProductos, eliminarProducto y obtenerProductoPorId en ProductoService

--- src/main/java/com/negocio/adris/model/service/ProductoService.java
+++ src/main/java/com/negocio/adris/model/service/ProductoService.java
@@ -4,6 +4,12 @@ import com.negocio.adris.model.entities.Producto;
 import com.negocio.adris.model.dtos.ProductoDto;
 import com.negocio.adris.model.exceptions.ProductoNotFoundException;
 
+import java.util.List;
+
 public interface ProductoService {
-    void crearProducto(ProductoDto productoDto) throws ProductoNotFoundException;
+    void crearProducto(ProductoDto productoDto);
+    void modificarProducto(int id, ProductoDto productoDto) throws ProductoNotFoundException;
+    List<Producto> obtenerTodosProductos();
+    void eliminarProducto(int id) throws ProductoNotFoundException;
+    Producto obtenerProductoPorId(int id) throws ProductoNotFoundException;
 }
