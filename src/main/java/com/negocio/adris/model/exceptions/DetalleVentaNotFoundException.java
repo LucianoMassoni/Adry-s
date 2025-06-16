70db4a630804730988562c09a4cebcc22d35c65d
Luciano Massoni <massoni.luciano7@gmail.com>
2025-06-16 18:43:02 -0300

    cree DetalleVentaNotFoundException

---
+++ src/main/java/com/negocio/adris/model/exceptions/DetalleVentaNotFoundException.java
@@ -0,0 +1,7 @@
+package com.negocio.adris.model.exceptions;
+
+public class DetalleVentaNotFoundException extends Exception {
+    public DetalleVentaNotFoundException(String message) {
+        super(message);
+    }
+}
