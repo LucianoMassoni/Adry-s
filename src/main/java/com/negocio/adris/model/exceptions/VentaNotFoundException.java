feeaf1fcf2a2865a8750cc2d24ea9c7b68342f88
Luciano Massoni <massoni.luciano7@gmail.com>
2025-06-16 18:35:28 -0300

    cree VentaNotFoundException

---
+++ src/main/java/com/negocio/adris/model/exceptions/VentaNotFoundException.java
@@ -0,0 +1,7 @@
+package com.negocio.adris.model.exceptions;
+
+public class VentaNotFoundException extends Exception {
+    public VentaNotFoundException(String message){
+        super(message);
+    }
+}
