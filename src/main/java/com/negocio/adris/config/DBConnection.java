539ae765d86c27df5e5585742d63bb9499d2e722
Luciano Massoni <massoni.luciano7@gmail.com>
2025-08-04 12:06:37 -0300

    feat(db): cambie la ubicación de la base de datos.

--- src/main/java/com/negocio/adris/config/DBConnection.java
+++ src/main/java/com/negocio/adris/config/DBConnection.java
@@ -1,15 +1,24 @@
 package com.negocio.adris.config;
 
 import com.google.inject.Provider;
-import com.google.inject.Singleton;
 
+import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 public class DBConnection implements Provider<Connection> {
-    private static final String URL = "jdbc:sqlite:fiambreria.db";
+//    private static final String URL = "jdbc:sqlite:fiambreria.db";
+
+    private static final String URL;
+
+    static {
+        String userHome = System.getProperty("user.home");
+        String appDir = userHome + File.separator + ".fiambreria";
+        new File(appDir).mkdirs(); // crea la carpeta si no existe
+        URL = "jdbc:sqlite:" + appDir + File.separator + "fiambreria.db";
+    }
 
     @Override
     public Connection get() {
