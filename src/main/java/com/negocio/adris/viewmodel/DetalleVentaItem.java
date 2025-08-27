17a81fbc6d90900e27f25f14cc6b276d9bee4032
Luciano Massoni <massoni.luciano7@gmail.com>
2025-08-27 15:51:12 -0300

    feat(DetalleVentaItem): calcula el precio por kg si el producto es divisible.

--- src/main/java/com/negocio/adris/viewmodel/DetalleVentaItem.java
+++ src/main/java/com/negocio/adris/viewmodel/DetalleVentaItem.java
@@ -13,12 +13,14 @@ import java.math.RoundingMode;
 public class DetalleVentaItem {
     private final ObjectProperty<Producto> producto = new SimpleObjectProperty<>();
     private final ObjectProperty<BigDecimal> cantidad = new SimpleObjectProperty<>();
+    private final ObjectProperty<UnidadMedida> unidadMedida = new SimpleObjectProperty<>();
     private final ObjectProperty<BigDecimal> descuento = new SimpleObjectProperty<>();
     private final ObjectProperty<BigDecimal> subtotal = new SimpleObjectProperty<>();
 
-    public DetalleVentaItem(Producto producto, BigDecimal cantidad, BigDecimal descuento){
+    public DetalleVentaItem(Producto producto, BigDecimal cantidad, UnidadMedida unidadMedida, BigDecimal descuento){
         this.producto.set(producto);
         this.cantidad.set(cantidad);
+        this.unidadMedida.set(unidadMedida);
         this.descuento.set(descuento);
         recalcSubtotal();
 
@@ -26,14 +28,25 @@ public class DetalleVentaItem {
         this.descuento.addListener((obs, oldv, newv) -> recalcSubtotal());}
 
     private void recalcSubtotal(){
-        BigDecimal precio = producto.get().getPrecio();
-        BigDecimal mult = BigDecimal.ONE.subtract(descuento.get().divide(BigDecimal.valueOf(100)));
-        subtotal.set(precio.multiply(cantidad.get()).multiply(mult)
-                .setScale(2, RoundingMode.HALF_UP));
+        if (producto.get().esDivisible()){
+            BigDecimal precioPorKg = producto.get().getPrecio().divide(BigDecimal.valueOf(producto.get().getPeso()));
+            BigDecimal cantidadCompradaKg = unidadMedida.get().equals(UnidadMedida.KILOS) ? cantidad.get() : cantidad.get().divide(BigDecimal.valueOf(1000));
+            BigDecimal precioPorCantidadComprada = cantidadCompradaKg.multiply(precioPorKg);
+
+            BigDecimal mult = BigDecimal.ONE.subtract(descuento.get().divide(BigDecimal.valueOf(100)));
+            subtotal.set(precioPorCantidadComprada.multiply(mult).setScale(2, RoundingMode.HALF_UP));
+
+        } else {
+            BigDecimal precio = producto.get().getPrecio();
+            BigDecimal mult = BigDecimal.ONE.subtract(descuento.get().divide(BigDecimal.valueOf(100)));
+            subtotal.set(precio.multiply(cantidad.get()).multiply(mult)
+                    .setScale(2, RoundingMode.HALF_UP));
+        }
     }
 
     public ObjectProperty<Producto> productoProperty(){ return producto; }
     public ObjectProperty<BigDecimal> cantidadProperty() { return cantidad; }
+    public ObjectProperty<UnidadMedida> unidadMedidaProperty() { return unidadMedida; }
     public ObjectProperty<BigDecimal> descuentoProperty() { return descuento; }
     public ObjectProperty<BigDecimal> subtotalProperty() { return subtotal; }
 }
