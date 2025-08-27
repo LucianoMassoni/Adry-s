7288e2bd034b84b5431e98e35b4b20c202f80259
Luciano Massoni <massoni.luciano7@gmail.com>
2025-08-27 12:07:29 -0300

    fix(DetalleVentaItem): cambie cantidad de int a BigDecimal

--- src/main/java/com/negocio/adris/viewmodel/DetalleVentaItem.java
+++ src/main/java/com/negocio/adris/viewmodel/DetalleVentaItem.java
@@ -1,6 +1,7 @@
 package com.negocio.adris.viewmodel;
 
 import com.negocio.adris.model.entities.Producto;
+import com.negocio.adris.model.enums.UnidadMedida;
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleIntegerProperty;
@@ -11,11 +12,11 @@ import java.math.RoundingMode;
 
 public class DetalleVentaItem {
     private final ObjectProperty<Producto> producto = new SimpleObjectProperty<>();
-    private final IntegerProperty cantidad = new SimpleIntegerProperty();
+    private final ObjectProperty<BigDecimal> cantidad = new SimpleObjectProperty<>();
     private final ObjectProperty<BigDecimal> descuento = new SimpleObjectProperty<>();
     private final ObjectProperty<BigDecimal> subtotal = new SimpleObjectProperty<>();
 
-    public DetalleVentaItem(Producto producto, int cantidad, BigDecimal descuento){
+    public DetalleVentaItem(Producto producto, BigDecimal cantidad, BigDecimal descuento){
         this.producto.set(producto);
         this.cantidad.set(cantidad);
         this.descuento.set(descuento);
@@ -27,13 +28,12 @@ public class DetalleVentaItem {
     private void recalcSubtotal(){
         BigDecimal precio = producto.get().getPrecio();
         BigDecimal mult = BigDecimal.ONE.subtract(descuento.get().divide(BigDecimal.valueOf(100)));
-        subtotal.set(precio.multiply(BigDecimal.valueOf(cantidad.get()).multiply(mult)
-                .setScale(2, RoundingMode.HALF_UP)));
-
+        subtotal.set(precio.multiply(cantidad.get()).multiply(mult)
+                .setScale(2, RoundingMode.HALF_UP));
     }
 
     public ObjectProperty<Producto> productoProperty(){ return producto; }
-    public IntegerProperty cantidadProperty() { return cantidad; }
+    public ObjectProperty<BigDecimal> cantidadProperty() { return cantidad; }
     public ObjectProperty<BigDecimal> descuentoProperty() { return descuento; }
     public ObjectProperty<BigDecimal> subtotalProperty() { return subtotal; }
 }
