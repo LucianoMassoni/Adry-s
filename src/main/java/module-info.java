45e4fbd12e300afc8eb60aae6b416eb69137e4b2
Luciano Massoni <massoni.luciano7@gmail.com>
2025-09-08 12:02:11 -0300

    feat(pom-moduleinfo): config.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -8,14 +8,19 @@ module com.negocio.adris {
     requires jakarta.inject;
     requires com.google.guice;
     requires org.hibernate.validator;
+    requires java.desktop;
     opens com.negocio.adris.model.dtos to org.hibernate.validator;
 
-    opens com.negocio.adris to javafx.fxml;
     exports com.negocio.adris;
 
     exports com.negocio.adris.model.repositories;
     exports com.negocio.adris.model.service;
     exports com.negocio.adris.model.entities;
+    exports com.negocio.adris.viewmodel;
+
+    exports com.negocio.adris.model.dtos;
+
+    exports com.negocio.adris.model.exceptions;
 
     exports com.negocio.adris.config to com.google.guice;
     exports com.negocio.adris.model.enums;
