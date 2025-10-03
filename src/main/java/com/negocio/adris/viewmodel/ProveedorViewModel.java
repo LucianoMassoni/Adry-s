baeafed61b9f8f79ed88b391497d59c5f6a9d813
Luciano Massoni <massoni.luciano7@gmail.com>
2025-10-02 22:28:53 -0300

    feat (ProveedorViewModel): hice el view model basico.

---
+++ src/main/java/com/negocio/adris/viewmodel/ProveedorViewModel.java
@@ -0,0 +1,71 @@
+package com.negocio.adris.viewmodel;
+
+import com.google.inject.Inject;
+import com.negocio.adris.model.dtos.ProveedorDto;
+import com.negocio.adris.model.entities.Proveedor;
+import com.negocio.adris.model.exceptions.ProveedorNotFoundException;
+import com.negocio.adris.model.service.ProveedorService;
+import javafx.beans.property.*;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+public class ProveedorViewModel {
+    private final ProveedorService proveedorService;
+
+    private final LongProperty id = new SimpleLongProperty();
+    private final StringProperty nombre = new SimpleStringProperty();
+    private final StringProperty telefono = new SimpleStringProperty();
+
+    private final ObservableList<Proveedor> proveedores = FXCollections.observableArrayList();
+    @Inject
+    public ProveedorViewModel(ProveedorService proveedorService){
+        this.proveedorService = proveedorService;
+    }
+
+    public void limpiarFormulario(){
+        nombre.set(null);
+        telefono.set(null);
+    }
+
+    public void cargarProveedores() {
+        proveedores.setAll(proveedorService.getProveedores());
+    }
+
+    public ObservableList<Proveedor> getProveedores(){
+        return proveedores;
+    }
+
+    public void cargarProveedor(Proveedor p){
+        id.set(p.getId());
+        nombre.set(p.getNombre());
+        telefono.set(p.getTelefono());
+    }
+
+    public void crearProveedor(){
+        ProveedorDto dto = new ProveedorDto(
+                nombre.get(),
+                telefono.get()
+        );
+
+        proveedorService.guardar(dto);
+    }
+
+    public void modificarProveedor() throws ProveedorNotFoundException {
+        ProveedorDto dto = new ProveedorDto(
+                nombre.get(),
+                telefono.get()
+        );
+
+        proveedorService.modificar(id.get(), dto);
+    }
+
+    public void eliminarProveedor() throws ProveedorNotFoundException {
+        proveedorService.eliminar(id.get());
+    }
+
+
+    public LongProperty idProperty() {return id; }
+    public StringProperty nombreProperty() { return nombre; }
+    public StringProperty telefonoProperty() { return telefono; }
+    public ObservableList<Proveedor> proveedorsProperty() { return proveedores; }
+}
