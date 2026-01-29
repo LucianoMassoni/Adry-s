1ab6e2511359fe597948b1a667fbb99f91aab9f0
Luciano Massoni <massoni.luciano7@gmail.com>
2026-01-29 20:40:21 -0300

    feat(Utils): agregue funcionse de formateo de tipo en Utils.

--- src/main/java/com/negocio/adris/utils/Utils.java
+++ src/main/java/com/negocio/adris/utils/Utils.java
@@ -1,5 +1,11 @@
 package com.negocio.adris.utils;
 
+import java.math.BigDecimal;
+import java.text.NumberFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
 public abstract class Utils {
     public static String capitalize(String str){
         if (str == null || str.isEmpty()){
@@ -7,5 +13,18 @@ public abstract class Utils {
             return str;
         }
         return str.substring(0, 1).toUpperCase() + str.substring(1);
+    }
+
+    public static String bigDecimalFormatter(BigDecimal n){
+        NumberFormat format = NumberFormat.getInstance(new Locale("es", "AR"));
+        format.setMinimumFractionDigits(2);
+        format.setMaximumFractionDigits(2);
+
+        return format.format(n);
+    }
+
+    public static String dateTimeFormatter(LocalDate date){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yy");
+        return date.format(formatter);
     }
 }
