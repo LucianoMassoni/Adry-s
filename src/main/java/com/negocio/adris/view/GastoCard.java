0e44eb30473727120d32968b2002e26529bbb97e
Luciano Massoni <massoni.luciano7@gmail.com>
2026-01-29 20:34:24 -0300

    feat(gastoCard): agregué historial de gastos en el mismo GastoCard y eliminé GastoPagoCard

--- src/main/java/com/negocio/adris/view/GastoCard.java
+++ src/main/java/com/negocio/adris/view/GastoCard.java
@@ -1,7 +1,9 @@
 package com.negocio.adris.view;
 
 import com.negocio.adris.model.entities.Gasto;
+import com.negocio.adris.model.entities.Pago;
 import com.negocio.adris.utils.BotonAfirmar;
+import com.negocio.adris.utils.Utils;
 import javafx.animation.FadeTransition;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
@@ -9,15 +11,18 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.*;
 import javafx.util.Duration;
 
+import java.math.BigDecimal;
 import java.time.format.DateTimeFormatter;
 import java.util.function.Consumer;
 
-public class GastoCard extends HBox {
+public class GastoCard extends VBox {
     private Consumer<Gasto> onEditar;
     private Consumer<Gasto> onPagar;
     private boolean seleccionado = false;
     private final HBox buttonHolder;
     private boolean modoLectura;
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yy");
+
 
     public void setOnEditar(Consumer<Gasto> onEditar) { this.onEditar = onEditar; }
 
@@ -31,19 +36,56 @@ public class GastoCard extends HBox {
         if (seleccionado) {
             if (!getStyleClass().contains("gasto-card-seleccionado")) {
                 getStyleClass().add("gasto-card-seleccionado");
-                fadeNode(buttonHolder, true);
+                if (modoLectura) {
+                    buttonHolder.setVisible(false);
+                } else {
+                    fadeNode(buttonHolder, true);
+                }
+                construirPago();
+                expandir();
             }
         } else {
             getStyleClass().remove("gasto-card-seleccionado");
-            fadeNode(buttonHolder, false);
+            if (modoLectura) {
+                buttonHolder.setVisible(false);
+            } else {
+                fadeNode(buttonHolder, false);
+            }
+            colapsar();
         }
+
+    }
+
+    private void expandir() {
+        pagoBox.setManaged(true);
+        pagoBox.setVisible(true);
+        pagoBoxTituloHolder.setManaged(true);
+        pagoBoxTituloHolder.setVisible(true);
+
+        FadeTransition ft = new FadeTransition(Duration.millis(200), pagoBox);
+        ft.setFromValue(0);
+        ft.setToValue(1);
+        ft.play();
+    }
+
+    private void colapsar() {
+        FadeTransition ft = new FadeTransition(Duration.millis(150), pagoBox);
+        ft.setFromValue(1);
+        ft.setToValue(0);
+        ft.setOnFinished(e -> {
+            pagoBox.setVisible(false);
+            pagoBox.setManaged(false);
+            pagoBoxTituloHolder.setManaged(false);
+            pagoBoxTituloHolder.setVisible(false);
+        });
+        ft.play();
     }
 
     private FadeTransition fadeActual;
 
     private void fadeNode(Node node, boolean fadeIn) {
         if (fadeActual != null) fadeActual.stop(); // corta el fade anterior
-
+        
         fadeActual = new FadeTransition(Duration.millis(200), node);
         if (fadeIn) {
             node.setVisible(true);
@@ -64,12 +106,21 @@ public class GastoCard extends HBox {
 
     private Gasto gasto;
 
-    private  Label fechaContraida = new Label();
-    private  Label fechaSaldar = new Label();
-    private  Label proveedorNombre = new Label();
-    private  Label proveedorTelefono = new Label();
-    private  Label nota = new Label();
-    private  Label monto = new Label();
+    private Label fechaContraida = new Label();
+    private Label fechaSaldar = new Label();
+    private Label proveedorNombre = new Label();
+    private Label proveedorTelefono = new Label();
+    private Label nota = new Label();
+    private Label monto = new Label();
+    private Label montoRestante = new Label();
+
+    private VBox pagoFechaBox = new VBox();
+    private VBox pagoMontoBox = new VBox();
+    private VBox pagoMontoRestanteBox = new VBox();
+    private HBox pagoBox = new HBox();
+    private HBox pagoBoxTituloHolder = new HBox();
+
+
 
     public GastoCard(Gasto gasto, boolean modoLectura){
         this.gasto = gasto;
@@ -84,8 +135,6 @@ public class GastoCard extends HBox {
         fechaTituloHolder.getStyleClass().add("GC-tituloHolder");
         fechaTitulo.getStyleClass().add("GC-titulo");
         HBox fechasHolder = new HBox();
-
-
 
         VBox fechaContraidaBox = new VBox();
         Label fechaContraidaLabel = new Label("Fecha contraida");
@@ -126,6 +175,8 @@ public class GastoCard extends HBox {
         notaHolder.getStyleClass().add("GC-contentHolder");
         notaHolder.setMaxWidth(Double.MAX_VALUE);
         notaBox.getChildren().addAll(notaTituloHolder, notaHolder);
+        nota.setWrapText(true);
+        nota.setMaxWidth(120);
 
         // monto
         VBox montoBox = new VBox();
@@ -138,6 +189,18 @@ public class GastoCard extends HBox {
         montoHolder.getStyleClass().add("GC-contentHolder");
         montoHolder.setMaxWidth(Double.MAX_VALUE);
         montoBox.getChildren().setAll(montoTituloHolder, montoHolder);
+
+        // montoRestante
+        VBox montoRestanteBox = new VBox();
+        Label montoRestanteTitulo = new Label("Monto restante");
+        HBox montoRestanteTituloHolder = new HBox(montoRestanteTitulo);
+        montoRestanteTituloHolder.getStyleClass().add("GC-tituloHolder");
+        montoRestanteTituloHolder.setMaxWidth(Double.MAX_VALUE);
+        montoRestanteTitulo.getStyleClass().add("GC-titulo");
+        HBox montoRestanteHolder = new HBox(montoRestante);
+        montoRestanteHolder.getStyleClass().add("GC-contentHolder");
+        montoRestanteHolder.setMaxWidth(Double.MAX_VALUE);
+        montoRestanteBox.getChildren().setAll(montoRestanteTituloHolder, montoRestanteHolder);
 
         actualizarDatos(gasto);
 
@@ -163,14 +226,78 @@ public class GastoCard extends HBox {
             }
         });
 
+        // pagoFecha
+        Label pagoFechaTitulo = new Label("Fecha");
+        HBox pagoFechaTitulHolder = new HBox(pagoFechaTitulo);
+        pagoFechaTitulHolder.getStyleClass().add("GC-tituloHolder");
+        pagoFechaTitulHolder.setMaxWidth(Double.MAX_VALUE);
+        pagoFechaTitulo.getStyleClass().add("GC-titulo");
+
+        pagoFechaBox.getChildren().add(pagoFechaTitulHolder);
+
+        // pagoMonto
+        Label pagoMontoTitulo = new Label("Monto pagado");
+        HBox pagoMontoTituloHolder = new HBox(pagoMontoTitulo);
+        pagoMontoTituloHolder.getStyleClass().add("GC-tituloHolder");
+        pagoMontoTituloHolder.setMaxWidth(Double.MAX_VALUE);
+        pagoMontoTitulo.getStyleClass().add("GC-titulo");
+
+        pagoMontoBox.getChildren().add(pagoMontoTituloHolder);
+
+        // pagoMontoRestante
+        Label pagoMontoRestanteTitulo = new Label("Monto restante");
+        HBox pagoMontoRestanteTituloHolder = new HBox(pagoMontoRestanteTitulo);
+        pagoMontoRestanteTituloHolder.getStyleClass().add("GC-tituloHolder");
+        pagoMontoRestanteTituloHolder.setMaxWidth(Double.MAX_VALUE);
+        pagoMontoRestanteTitulo.getStyleClass().add("GC-titulo");
+
+        pagoMontoRestanteBox.getChildren().add(pagoMontoRestanteTituloHolder);
+
+
+        //pagobox
+        Region rp1 = new Region();
+        Region rp2 = new Region();
+        Region rp3 = new Region();
+        Region rp4 = new Region();
+
+        Label pagoBoxTitulo = new Label("Pagos");
+        pagoBoxTituloHolder.getChildren().add(pagoBoxTitulo);
+        pagoBoxTituloHolder.getStyleClass().add("GC-tituloHolder");
+        pagoBoxTituloHolder.setMaxWidth(Double.MAX_VALUE);
+        pagoBoxTitulo.getStyleClass().add("GC-titulo");
+
+        pagoBox.getChildren().addAll(
+                rp1,
+                pagoFechaBox,
+                rp2,
+                pagoMontoBox,
+                rp3,
+                pagoMontoRestanteBox,
+                rp4
+        );
+
+        pagoBox.getChildren().forEach(chil ->{
+            if (chil instanceof Region r){
+                r.setMaxWidth(Double.MAX_VALUE);
+                HBox.setHgrow(r, Priority.ALWAYS);
+            }
+        });
+
         // espacios
         Region r1 = new Region();
         Region r2 = new Region();
         Region r3 = new Region();
         Region r4 = new Region();
+        Region r5 = new Region();
+
+        HBox r6 = new HBox();
+        r6.setMaxWidth(Double.MAX_VALUE);
+
+        StackPane stackPane = new StackPane();
+        stackPane.getChildren().addAll(r6, buttonHolder);
 
 
-        this.getChildren().addAll(
+        HBox gastoBox = new HBox(
                 fechaBox,
                 r1,
                 proveedorBox,
@@ -179,26 +306,73 @@ public class GastoCard extends HBox {
                 r3,
                 montoBox,
                 r4,
-                modoLectura ? new Region() : buttonHolder
+                montoRestanteBox,
+                r5,
+                stackPane
         );
 
-        this.getChildren().forEach(chil ->{
+        gastoBox.getChildren().forEach(chil ->{
             if (chil instanceof Region r){
                 r.setMaxWidth(Double.MAX_VALUE);
                 HBox.setHgrow(r, Priority.ALWAYS);
             }
         });
+
+        r6.setMinWidth(buttonHolder.getWidth());
+
+
+        pagoBoxTituloHolder.setVisible(false);
+        pagoBoxTituloHolder.setManaged(false);
+        pagoBox.setVisible(false);
+        pagoBox.setManaged(false);
+
+        this.getChildren().addAll(
+                gastoBox,
+                pagoBoxTituloHolder,
+                pagoBox
+        );
     }
 
     public void actualizarDatos(Gasto gasto) {
         this.gasto = gasto;
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yy");
 
         fechaContraida.setText(gasto.getFechaDeudaContraida().format(formatter));
         fechaSaldar.setText(gasto.getFechaVencimiento().format(formatter));
         proveedorNombre.setText(gasto.getProveedor().getNombre());
         proveedorTelefono.setText("Tel. " + gasto.getProveedor().getTelefono());
         nota.setText(gasto.getNota());
-        monto.setText("$" + gasto.getMontoRestante());
+        monto.setText("$" + Utils.bigDecimalFormatter(gasto.getMonto()));
+        montoRestante.setText("$" + Utils.bigDecimalFormatter(gasto.getMontoRestante()));
+    }
+
+    private void construirPago(){
+        BigDecimal montoRestante = gasto.getMonto();
+
+        for (Pago pago : gasto.getPagos()){
+            // pagoFecha
+            Label fecha = new Label(pago.getFechaPago().format(formatter));
+            HBox pagoFechaHolder = new HBox(fecha);
+            pagoFechaHolder.getStyleClass().add("GC-contentHolder");
+            pagoFechaHolder.setMaxWidth(Double.MAX_VALUE);
+
+            pagoFechaBox.getChildren().add(pagoFechaHolder);
+
+            // pagoMonto
+            Label pagoMonto = new Label("$" + Utils.bigDecimalFormatter(pago.getMontoPagado()));
+            HBox pagoMontoHolder = new HBox(pagoMonto);
+            pagoMontoHolder.getStyleClass().add("GC-contentHolder");
+            pagoMontoHolder.setMaxWidth(Double.MAX_VALUE);
+
+            pagoMontoBox.getChildren().add(pagoMontoHolder);
+
+            montoRestante = montoRestante.subtract(pago.getMontoPagado());
+            // pagoMontoRestante
+            Label pagoMontoRestante = new Label("$" + Utils.bigDecimalFormatter(montoRestante));
+            HBox pagoMontoRestanteHolder = new HBox(pagoMontoRestante);
+            pagoMontoRestanteHolder.getStyleClass().add("GC-contentHolder");
+            pagoMontoRestanteHolder.setMaxWidth(Double.MAX_VALUE);
+
+            pagoMontoRestanteBox.getChildren().add(pagoMontoRestanteHolder);
+        }
     }
 }
